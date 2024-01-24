d4b7da11c1d9222ee8123f722512b2c1343c261a
hoaturi <oink2716@gmail.com>
2024-01-25 03:46:44 +0900

    feat: additional exception for email and jwt validation

---
+++ jobBoard/Common/Exceptions/InvalidJwtException.cs
@@ -0,0 +1,6 @@
+﻿using System.Net;
+
+namespace JobBoard;
+
+public class InvalidJwtException()
+    : CustomException(ErrorCodes.InvalidToken, HttpStatusCode.Unauthorized, "Invalid jwt token");
---
+++ jobBoard/Infrastructure/Services/EmailService/EmailSendFailedException.cs
@@ -0,0 +1,10 @@
+﻿using System.Net;
+
+namespace JobBoard;
+
+public class EmailSendFailedException()
+    : CustomException(
+        ErrorCodes.EmailSendFailed,
+        HttpStatusCode.InternalServerError,
+        "Failed to send email."
+    );
