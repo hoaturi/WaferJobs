479c6a8763c1d0dd1748248c64bb36145df89bdf
hoaturi <oink2716@gmail.com>
2024-01-28 01:08:16 +0900

    refactor: uses primary constructor

--- jobBoard/Common/Middlewares/ExceptionHandlingMiddleware.cs
+++ jobBoard/Common/Middlewares/ExceptionHandlingMiddleware.cs
@@ -2,14 +2,9 @@
 
 namespace JobBoard;
 
-public class ExceptionHandlingMiddleware : IMiddleware
+public class ExceptionHandlingMiddleware(ILogger<ExceptionHandlingMiddleware> logger) : IMiddleware
 {
-    private readonly ILogger<ExceptionHandlingMiddleware> _logger;
-
-    public ExceptionHandlingMiddleware(ILogger<ExceptionHandlingMiddleware> logger)
-    {
-        _logger = logger;
-    }
+    private readonly ILogger<ExceptionHandlingMiddleware> _logger = logger;
 
     public async Task InvokeAsync(HttpContext context, RequestDelegate next)
     {
