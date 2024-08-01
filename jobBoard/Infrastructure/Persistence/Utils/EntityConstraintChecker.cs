344dc6e01d1c61f1c2383c82311be75c13d37195
hoaturi <oink2716@gmail.com>
2024-08-01 21:29:30 +0900

    Add unique constraint violation checker
    -Implement a checker to identify the specific column name that caused a unique constraint violation

---
+++ jobBoard/Infrastructure/Persistence/Utils/EntityConstraintChecker.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Npgsql;
+
+namespace JobBoard.Infrastructure.Persistence.Utils;
+
+public class EntityConstraintChecker(ILogger<EntityConstraintChecker> logger)
+{
+    public bool IsUniqueConstraintViolation<TEntity>(DbContext dbContext, string propertyName, string errorCode,
+        string? constraintName)
+        where TEntity : class
+    {
+        if (errorCode != PostgresErrorCodes.UniqueViolation) return false;
+
+        var index = dbContext.Model.FindEntityType(typeof(TEntity))
+            ?.GetIndexes()
+            .FirstOrDefault(i => i.IsUnique && i.Properties.Any(p => p.Name == propertyName));
+
+        var databaseIndexName = index?.GetDatabaseName();
+        logger.LogDebug("{EntityName} unique index name for {PropertyName}: {IndexName}",
+            typeof(TEntity).Name, propertyName, databaseIndexName);
+
+        return databaseIndexName == constraintName;
+    }
+}
