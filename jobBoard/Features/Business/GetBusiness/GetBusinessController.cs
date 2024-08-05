5c0cc31d34e1e7d0b953421fce3c5afb89a041ce
hoaturi <oink2716@gmail.com>
2024-08-06 02:58:47 +0900

    Refactor use primary constructor

--- jobBoard/Features/Business/GetBusiness/GetBusinessController.cs
+++ jobBoard/Features/Business/GetBusiness/GetBusinessController.cs
@@ -9,12 +9,10 @@ namespace JobBoard.Features.Business.GetBusiness;
 [Route("api/businesses")]
 public class GetBusinessController(ISender sender) : ControllerBase
 {
-    private readonly ISender _sender = sender;
-
     [HttpGet("{id:guid}")]
     public async Task<IActionResult> GetBusiness(Guid id)
     {
-        var result = await _sender.Send(new GetBusinessQuery(id));
+        var result = await sender.Send(new GetBusinessQuery(id));
 
         return !result.IsSuccess ? this.HandleError(result.Error) : Ok(result.Value);
     }
