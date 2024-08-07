4b8b57f8f857f66260a4fdfeaeaa01e9ec938840
hoaturi <oink2716@gmail.com>
2024-08-07 21:26:04 +0900

    Refactor cache key names for consistency

--- jobBoard/Common/Constants/CacheKeys.cs
+++ jobBoard/Common/Constants/CacheKeys.cs
@@ -3,10 +3,10 @@
 public static class CacheKeys
 {
     public const string RevokedToken = "revokedToken:";
-    public const string CountriesCacheKey = "countries:WithJobPosts";
-    public const string CitiesCacheKey = "cities:WithJobPosts";
-    public const string LocationsCacheKey = "locations:WithJobPosts";
+    public const string CountriesCacheKey = "countries";
+    public const string CitiesCacheKey = "cities";
+    public const string LocationsCacheKey = "locations";
     public const string PopularKeywordsCacheKey = "popularKeywords";
     public const string JobCountCacheKey = "totalJobPostCount";
-    public const string CurrencyExchangeRates = "CurrencyExchangeRates";
+    public const string CurrencyExchangeRates = "currencyExchangeRates";
 }
