8db8a0ee6575c9290152d6dd30b094d71e38d40c
hoaturi <oink2716@gmail.com>
2024-08-21 11:12:14 +0900

    Add GuidExtensions for Base64 string conversions

---
+++ jobBoard/Common/Extensions/GuidExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace JobBoard.Common.Extensions;
+
+public static class GuidExtensions
+{
+    public static string ToBase64String(this Guid guid)
+    {
+        return Convert.ToBase64String(guid.ToByteArray());
+    }
+
+    public static string NewBase64Guid()
+    {
+        return Guid.NewGuid().ToBase64String();
+    }
+}
