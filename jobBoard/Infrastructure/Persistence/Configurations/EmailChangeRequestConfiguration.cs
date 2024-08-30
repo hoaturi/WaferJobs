a0ed7deed47a6b67f2398d841ac4616cb38e95f9
hoaturi <oink2716@gmail.com>
2024-08-30 17:58:22 +0900

    Add EmailChangeRequestConfiguration

---
+++ jobBoard/Infrastructure/Persistence/Configurations/EmailChangeRequestConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using JobBoard.Domain.Auth;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace JobBoard.Infrastructure.Persistence.Configurations;
+
+public class EmailChangeRequestConfiguration : IEntityTypeConfiguration<EmailChangeRequestEntity>
+{
+    public void Configure(EntityTypeBuilder<EmailChangeRequestEntity> builder)
+    {
+        builder.HasIndex(ecr => ecr.Pin)
+            .IsUnique();
+
+        builder.HasIndex(ecr => ecr.ExpiresAt);
+        builder.HasIndex(ecr => ecr.IsVerified);
+
+        builder.HasOne(ecr => ecr.User)
+            .WithMany()
+            .HasForeignKey(ecr => ecr.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
