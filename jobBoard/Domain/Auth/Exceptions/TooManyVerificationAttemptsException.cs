5a95fd01554d06844a5f2ca2619585e2981317c4
hoaturi <oink2716@gmail.com>
2024-09-01 03:52:19 +0900

    Relocate to Auth directory

---
+++ jobBoard/Domain/Auth/Exceptions/TooManyVerificationAttemptsException.cs
@@ -0,0 +1,11 @@
+﻿using System.Net;
+using JobBoard.Common.Constants;
+using JobBoard.Common.Exceptions;
+
+namespace JobBoard.Domain.Auth.Exceptions;
+
+public class TooManyVerificationAttemptsException(Guid userId) : CustomException(
+    ErrorCodes.TooManyVerificationAttempts,
+    HttpStatusCode.BadRequest,
+    $"User {userId} has exceeded the maximum number of verification attempts."
+);
