a69d21f71a77e5e152dc59d9d1ae33983c4059e2
hoaturi <oink2716@gmail.com>
2024-10-15 01:51:14 +0900

    Fix removed unused MessagePack

--- jobBoard/Domain/Common/CountryEntity.cs
+++ jobBoard/Domain/Common/CountryEntity.cs
@@ -1,15 +1,9 @@
-﻿using MessagePack;
+﻿namespace JobBoard.Domain.Common;
 
-namespace JobBoard.Domain.Common;
-
-[MessagePackObject]
 public class CountryEntity
 {
-    [Key(0)] public int Id { get; set; }
-
-    [Key(1)] public required string Label { get; init; }
-
-    [Key(2)] public required string Code { get; init; }
-
-    [Key(3)] public required string Slug { get; init; }
+    public int Id { get; set; }
+    public required string Label { get; init; }
+    public required string Code { get; init; }
+    public required string Slug { get; init; }
 }
